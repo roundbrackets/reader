08ccf61c483cdaf97bd9610ec96e32475796b57a
Tina Gunnarsson <roundbrackets@gmail.com>
2026-03-27 15:20:51 -0700

    use systemd-run instead of nohup

--- quick.sh
+++ quick.sh
@@ -1,4 +1,4 @@
-#/!/bin/bash
+#!/bin/bash
 
 multipass stop reader
 multipass delete reader
@@ -6,5 +6,5 @@ multipass purge
 multipass launch --name reader --cpus 1 --memory 512M --disk 4G --network en0 20.04
 multipass mount ./reader reader:/home/ubuntu/reader
 multipass exec reader -- bash -c "cd reader && ./install.sh"
-multipass exec reader -- bash -c "cd reader && nohup ./run.sh &"
+multipass exec reader -- sudo systemd-run --no-block bash -c "cd /home/ubuntu/reader && ./run.sh"
 multipass info reader
